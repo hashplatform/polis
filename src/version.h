ce9300dcff869221b0e7fa56dacd0fdf3b0f97c3
Enrique Berrueta <eabz@polispay.org>
2019-10-11 10:01:56 -0500

    Revert message switch, bump and ban

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70218;
+static const int PROTOCOL_VERSION = 70219;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70217;
+static const int MIN_PEER_PROTO_VERSION = 70218;
 
 //! disconnect from peers older than this proto version when DIP3 is activated via the BIP9 deployment
 static const int MIN_PEER_PROTO_VERSION_DIP3 = 70219;
